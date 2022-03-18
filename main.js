4154f2cc43c14a2671476e0b8db9e365b4eafb7b
dovilegirdziu <97856821+dgstudiolt@users.noreply.github.com>
2022-03-18 02:09:55 +0200

    taking care of the 'notch'

--- main.js
+++ main.js
@@ -29,3 +29,7 @@ document.addEventListener('keydown', function playSound(e) {
     })
   });
 
+// fitting with the notch
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
